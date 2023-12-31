eac15d24999fb585dc0f3092d8ad9eb09b82b1e8
Musiemh <musiemh101@gmail.com>
2023-12-31 16:18:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './Firebase/Firebase'; 
+import { useEffect, useState } from 'react';
+import { doc, addDoc } from "firebase/firestore"; 
 
 function App() {
+  //init var
+  const [account,setAccount] = useState("")
+  const [fname,setfname] = useState("")
+  const [mname,setmname] = useState("")
+  const [lname,setlname] = useState("")
+
+  const featch = async () => {
+    const querySnapshot = await getDocs(collection(db, "Users"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  }
+
+  useEffect (() => {
+featch();
+  },[])
+
+ const add = async (e) => {
+  e.preventDefault();
+//   await addDoc(doc(db, "Users"), {
+//     account_type: account,
+//     first_name: fname,
+//     last_name: lname,
+//     middle_name: mname,
+//   });
+//  }
+const docRef = await addDoc(collection(db, "Users"), {
+  account_type: account,
+    first_name: fname,
+    last_name: lname,
+    middle_name: mname,
+});
+console.log("Document written with ID: ", docRef.id);
+alert("Document written with ID: ", docRef.id)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>new code</code> and save to reload.
@@ -17,7 +57,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className='stat'>comming soon</div>
+      <div className='Add'>
+        <form onSubmit={add}>
+          <input type='text' placeholder='account type' onChange={(event) => {setAccount(event.target.value)}}></input>
+          <input type='text' placeholder='fn' onChange={(event) => {setfname(event.target.value)}}></input>
+          <input type='text' placeholder='mn' onChange={(event) => {setmname(event.target.value)}}></input>
+          <input type='text' placeholder='lm' onChange={(event) => {setlname(event.target.value)}}></input>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Aj6IMt6gVvwkKCwesXt0SGNjZwQKBSE",
+  authDomain: "unityclassfinder.firebaseapp.com",
+  projectId: "unityclassfinder",
+  storageBucket: "unityclassfinder.appspot.com",
+  messagingSenderId: "445589944202",
+  appId: "1:445589944202:web:ccd46f3b178d9231ee7a73",
+  measurementId: "G-GY8B3ZDT0J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+export default app;
